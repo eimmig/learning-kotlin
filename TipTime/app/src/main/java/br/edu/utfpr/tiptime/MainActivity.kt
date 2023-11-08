efda0055287385bc8202e7fc43c9208a29cd600d
eimmig <eduardo-immig@hotmail.com>
2023-11-08 20:44:47 -0300

    Ajustes e implementação do vétodo de calcular o tip

--- TipTime/app/src/main/java/br/edu/utfpr/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/br/edu/utfpr/tiptime/MainActivity.kt
@@ -1,7 +1,12 @@
 package br.edu.utfpr.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import br.edu.utfpr.tiptime_2023_2.R
 import br.edu.utfpr.tiptime_2023_2.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -19,8 +24,27 @@ class MainActivity : AppCompatActivity() {
             calculateTip()
         }
 
-        binding.tipsResult.text = getString(R.string.tip_amount_s, "0")
+        binding.costOfService.setOnKeyListener(View.OnKeyListener{ view, i, _ -> handleKeyEvent(view, i)
 
+        })
+
+        if (savedInstanceState != null) {
+            val tipResult = savedInstanceState.getString("tip_result")
+            binding.tipsResult.text = tipResult
+        } else {
+            binding.tipsResult.text = getString(R.string.tip_amount_s, "0")
+
+        }
+    }
+
+    private fun handleKeyEvent(view: View?, keycode: Int): Boolean {
+        if (keycode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
+            return true
+        } else {
+            return false
+        }
     }
 
     private fun calculateTip() {
@@ -28,8 +52,7 @@ class MainActivity : AppCompatActivity() {
 
         val cost = stringInTextField.toDoubleOrNull() ?: return
 
-        val selectedId = binding.tipOption.checkedRadioButtonId
-        val tipPercentage = when ( selectedId ) {
+        val tipPercentage = when (binding.tipOption.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else ->  0.15
@@ -46,5 +69,11 @@ class MainActivity : AppCompatActivity() {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
 
         binding.tipsResult.text = getString(R.string.tip_amount_s, formattedTip)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("tip_result", binding.tipsResult.text.toString())
     }
 }
