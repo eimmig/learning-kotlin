0180450555d82b6eaecf5a0e57b66c0a055aaf87
eimmig <eduardo-immig@hotmail.com>
2023-11-22 20:50:46 -0300

    Implementado projeto sqLite completo

---
+++ UsandoSQLite-Pm25S-2023/app/src/main/java/com/example/usandosqlite_pm25s_2023/ListarActivity.kt
@@ -0,0 +1,46 @@
+package com.example.usandosqlite_pm25s_2023
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SimpleCursorAdapter
+import com.example.usandosqlite_pm25s_2023.adapter.MeuAdapter
+import com.example.usandosqlite_pm25s_2023.database.DatabaseHandler
+import com.example.usandosqlite_pm25s_2023.entity.Pessoa
+
+class ListarActivity : AppCompatActivity() {
+
+    private lateinit var lvPrincipal : ListView
+
+    private lateinit var banco : DatabaseHandler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listar)
+
+        banco = DatabaseHandler( this )
+
+        lvPrincipal = findViewById( R.id.lvPrincipal )
+
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val cursor = banco.listCursor()
+
+        val adapter = MeuAdapter( this, cursor )
+
+        lvPrincipal.adapter = adapter
+    }
+
+    fun btIncluirOnClick(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+}
--- UsandoSQLite-Pm25S-2023/app/src/main/java/com/example/usandosqlite_pm25s_2023/MainActivity.kt
+++ UsandoSQLite-Pm25S-2023/app/src/main/java/com/example/usandosqlite_pm25s_2023/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.usandosqlite_pm25s_2023
 
+import android.content.ContentValues
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.example.usandosqlite_pm25s_2023.Database.DatabaseHandler
-import com.example.usandosqlite_pm25s_2023.Entity.Pessoa
+import com.example.usandosqlite_pm25s_2023.database.DatabaseHandler
+import com.example.usandosqlite_pm25s_2023.entity.Pessoa
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,49 +29,65 @@ class MainActivity : AppCompatActivity() {
         etNome = findViewById( R.id.etNome )
         etTelefone = findViewById( R.id.etTelefone )
 
-        banco = DatabaseHandler(this)
+        banco = DatabaseHandler( this )
+
+        if (intent.getIntExtra("cod", 0 ) != 0) {
+            val cod = intent.getIntExtra("cod", 0)
+            val nome = intent.getStringExtra("nome")
+            val telefone = intent.getStringExtra("telefone")
+
+            etCod.setText(cod.toString())
+            etNome.setText(nome)
+            etTelefone.setText(telefone)
+        } else {
+            val btExcluir = findViewById<Button>(R.id.btExcluir)
+            val btPesquiasr = findViewById<Button>(R.id.btPesquisar)
+
+            btExcluir.visibility = View.GONE
+            btPesquiasr.visibility = View.GONE
+        }
     }
 
-    fun btIncluirOnClick(view: View) {
-
-        val pessoa = Pessoa (0, etNome.text.toString(), etTelefone.text.toString())
-        banco.insert(pessoa)
-
-        Toast.makeText( this, "Inclusão realizada com sucesso", Toast.LENGTH_SHORT ).show()
-
-        limparTela()
-    }
     fun btAlterarOnClick(view: View) {
-        val pessoa = Pessoa (etCod.text.toString().toInt(), etNome.text.toString(), etTelefone.text.toString())
-        banco.update(pessoa)
 
-        Toast.makeText( this, "Alteração realizada com sucesso", Toast.LENGTH_SHORT ).show()
+        if (etCod.text.toString().isEmpty()) {
+            val pessoa = Pessoa( 0, etNome.text.toString(), etTelefone.text.toString() )
+
+            banco.insert( pessoa )
+
+            Toast.makeText( this, "Inclusão realizada com sucesso", Toast.LENGTH_SHORT ).show()
+
+            limparTela()
+        } else {
+            val pessoa = Pessoa( etCod.text.toString().toInt(), etNome.text.toString(), etTelefone.text.toString() )
+
+            banco.update( pessoa )
+
+            Toast.makeText( this, "Alteração realizada com sucesso", Toast.LENGTH_SHORT ).show()
+        }
+
+        finish()
+
     }
     fun btExcluirOnClick(view: View) {
-        val cod = etCod.text.toString().toInt()
 
-        banco.delete(cod)
+        banco.delete( etCod.text.toString().toInt() )
+
         Toast.makeText( this, "Exclusão realizada com sucesso", Toast.LENGTH_SHORT ).show()
+        finish()
     }
     fun btPesquisarOnClick(view: View) {
         val cod = etCod.text.toString().toInt()
 
-        val pessoa = banco.find(cod)
+        val pessoa = banco.find( cod )
 
-        if (pessoa != null) {
-            etNome.setText(pessoa.nome)
-            etTelefone.setText(pessoa.telefone)
+        if ( pessoa != null ) {
+            etNome.setText( pessoa.nome )
+            etTelefone.setText( pessoa.telefone )
         } else {
             Toast.makeText( this, "Registro não encontrado", Toast.LENGTH_SHORT ).show()
         }
     }
-    fun btListarOnClick(view: View) {
-
-        val saida = banco.list()
-
-        Toast.makeText( this, saida, Toast.LENGTH_SHORT ).show()
-    }
-
     fun limparTela() {
         etCod.setText( "" )
         etNome.setText( "" )
