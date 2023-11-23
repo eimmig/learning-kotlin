24ef861c085ef5b5ed14b79b4283cec4f1cb7a05
eimmig <eduardo-immig@hotmail.com>
2023-11-22 21:57:34 -0300

    Adicionado projeto frases desmotivacionais

---
+++ Affirmation/app/src/main/java/br/edu/utfpr/affirmation/MainActivity.kt
@@ -0,0 +1,22 @@
+package br.edu.utfpr.affirmation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.edu.utfpr.affirmation.adapter.ItemAdapter
+import br.edu.utfpr.affirmation.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myDataset = Datasource().loadAffirmation()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recrycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        recyclerView.setHasFixedSize(true)
+    }
+}
---
+++ Affirmation/app/src/main/java/br/edu/utfpr/affirmation/model/Affirmation.kt
@@ -0,0 +1,6 @@
+package br.edu.utfpr.affirmation.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation (@StringRes val stringResourceId : Int, @DrawableRes val imageResourceId : Int)
